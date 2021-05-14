a33ae997c8447a423d4251014f1a8b7ce13f1ab3
Ashish-kumar-pradhan <80833940+Ashish-kumar-pradhan@users.noreply.github.com>
2021-05-14 19:18:13 +0530

    DP

--- dp/MinStepsToGet1.java
+++ dp/MinStepsToGet1.java
@@ -15,19 +15,19 @@ public class MinStepsToGet1 {
 	
 
 	public static int countStepsR(int n) {
-		if(n==1) {
+		if(n == 1) {
 			return 0;
 		}
 		
 		int op1 = countStepsR(n - 1);
 		int minSteps = op1;
 
-		if(n%3==0) {
+		if(n%3 == 0) {
 		   int op2 = countStepsR(n/3);
 		   if(op2 < minSteps) minSteps = op2;
 		}
 		
-		if(n%2==0) {
+		if(n%2 == 0) {
 			int op3 = countStepsR(n/2) ;
 			if(op3 < minSteps) minSteps = op3;
 		}
@@ -35,10 +35,62 @@ public class MinStepsToGet1 {
 		return 1 + minSteps;
 
 	}
+	
+	public static int countStepsM(int n) {
+		int[] storage = new int[n + 1];
+		return countStepsM(n , storage);
+	}
+
+	private static int countStepsM(int n, int[] storage) {
+		if(n == 1) {
+			storage[n] = 0;
+			return storage[n];
+		}
+		if(storage[n] != 0) {
+			return storage[n];
+		}
+		int op1 = countStepsM(n - 1 , storage);
+		int minSteps = op1;
+
+		if(n%3 == 0) {
+		   int op2 = countStepsM(n/3 , storage);
+		   if(op2 < minSteps) minSteps = op2;
+		}
+		
+		if(n%2 == 0) {
+			int op3 = countStepsM(n/2 , storage) ;
+			if(op3 < minSteps) minSteps = op3;
+		}
+		
+		storage[n] = 1 + minSteps;
+		return storage[n];
+	}
+	
+	public static int countStepsDP(int n) {
+		int storage[] = new int[n + 1];
+		storage[1] = 0;
+		for(int i = 2; i <= n; i++) {
+			int min = storage[i - 1];
+			if(i % 3 == 0) {
+				if(min > storage[i / 3]) {
+					min = storage[i / 3];
+				}
+			}
+			if(i % 2 == 0) {
+				if(min > storage[i / 2]) {
+					min = storage[i / 2];
+				}
+			}
+			storage[i] = 1 + min;
+		}
+		return storage[n];
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		System.out.println(countStepsDP(10));
+		System.out.println(countStepsM(10));
 		System.out.println(countStepsR(10));
 
 	}
--- dp/MinimumNoOfSquare.java
+++ dp/MinimumNoOfSquare.java
@@ -2,19 +2,88 @@ package dp;
 
 public class MinimumNoOfSquare {
 	
+		
 	public static int minSqrR(int n) {
-		if(n==0) {
+		if(n == 0) {
 			return 0;
 		}
+		int min = Integer.MAX_VALUE;
 		int x = (int)Math.pow(n, 0.5);
-		
-		return 1 + minSqrR(n-x*x);
-		
+		for(int i = 1 ; i <= x ; i++) {
+			if(min > minSqrR(n-i*i)) {
+				min = minSqrR(n-i*i);
+			}
+		}
+		return 1 + min;
 	}
+	
+	public static int minSqrM(int n) {
+		int storage[] = new int[n+1];
+		return minSqrM(n , storage);
+	}
+	
+	public static int minSqrM(int n , int[] storage) {
+		if(n == 0) {
+			storage[n] = 0;
+			return storage[n];
+		}
+		if(storage[n] != 0) {
+			return storage[n];
+		}
+		int min = Integer.MAX_VALUE;
+		int x = (int)Math.pow(n, 0.5);
+		for(int i = 1 ; i <= x ; i++) {
+			if(min > minSqrM(n-i*i , storage)) {
+				min = minSqrM(n-i*i , storage);
+			}
+		}
+		storage[n] = 1 + min;
+		return storage[n];
+	}
+	
+	public static int minSqrDP(int n) {
+		int storage[] = new int[n+1];
+		
+		for(int i = 0; i <= n; i++) {
+			if(n < 4) {
+				storage[i] = i;
+			}
+			else {
+				storage[i] = i;
+				int x = (int)Math.pow(i, 0.5);
+				for(int j = 1 ; j <= x ; j++) {
+					storage[i] = Math.min(storage[i], 1 + storage[i - j*j]);
+	
+				}
+			}
+		}
+		
+		return storage[n];
+	}
+	
+	public static int minSqrLegendre(int n) {
+		
+		if((int)Math.sqrt(n)*(int)Math.sqrt(n) == n)  return 1;
+		
+		while(n % 4 == 0) {
+			n /= 4;
+		}
+		
+		if(n % 8 == 7) return 4 ;
+		
+		for(int i = 1 ; i < Math.sqrt(n) ; i++) {
+			if((int)Math.sqrt(n-i*i)*(int)Math.sqrt(n-i*i) == n-i*i) return 2 ;
+		}
+		return 3;
+	}
+		
 
 	public static void main(String[] args) {
 		 
-		System.out.println(minSqrR(23));
+		System.out.println(minSqrR(12));
+		System.out.println(minSqrM(12));
+		System.out.println(minSqrDP(12));
+		System.out.println(minSqrLegendre(12));
 
 	}
 
