afb27511a6e6b3813345e68fd6cf67d12a9a8689
unknown <ashish.pradhan.e21@nsut.ac.in>
2021-07-03 08:12:56 +0530

    BFS traversal Adjacent List Graph

--- graphAL/AdjacentList.java
+++ graphAL/AdjacentList.java
@@ -1,10 +1,40 @@
 package graphAL;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class AdjacentList {
 	
+	public static void printBFSHelper(ArrayList<ArrayList<Integer>> adj , int sv , boolean visited[]) {
+		Queue<Integer> q = new LinkedList<>();
+		q.add(sv);
+		int n = adj.size();
+		visited[sv] = true;
+		while(!q.isEmpty()) {
+			int front;
+			front = q.remove();
+			System.out.println(front);
+			ArrayList<Integer> list = adj.get(front);
+			for(int i = 0 ; i < list.size(); i++) {
+				if(!visited[list.get(i)]) {
+					q.add(list.get(i));
+					visited[list.get(i)] = true;
+				}
+			}
+		}
+	}
+	
+	public static void printBFS(ArrayList<ArrayList<Integer>> adj) {
+		boolean visited[] = new boolean[adj.size()];
+		for(int i = 0; i < adj.size() ; i++) {
+			if(!visited[i]) {
+				printBFSHelper(adj , i , visited);
+			}
+		}
+		
+	} 
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -13,10 +43,10 @@ public class AdjacentList {
 		Scanner s = new Scanner(System.in);
 		System.out.println("Enter the number of Elements");
 		n = s.nextInt();
-		ArrayList<ArrayList<Integer>> arr = new ArrayList<>();
+		ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
 		for(int i= 0 ; i < n ; i++) {
 			ArrayList<Integer> list = new ArrayList<>();
-			arr.add(list);
+			adj.add(list);
 		}
 		
 		for(int i= 0 ; i < n ; i++) {
@@ -26,11 +56,10 @@ public class AdjacentList {
 				if(temp == -1) {
 					break;
 				}
-				arr.get(i).add(temp);
+				adj.get(i).add(temp);
 			}
 		}
-		
-
+		printBFS(adj);
 	}
 
 }
